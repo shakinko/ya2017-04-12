9054cbfe58b3e093265e327ec39aeb6ce19bc628
akhmelev <akhmelev@gmail.com>
2017-05-03 21:11:15 +0300

    clawwork -06

---
+++ src/akhmelev/lesson02/tests.py
@@ -0,0 +1,69 @@
+from unittest import TestCase
+from .a_videoregistrator import naive_calc, fast_calc
+from .b_sheduler import Event, calcStartTimes
+from .c_knapsack import Item, calcMaxCost
+
+#############################################################################################
+#   Для тестов нажмите Ctrl+Shift+F10 установив курсор на  Run (или создайте конфигурацию)  #
+#############################################################################################
+class Run(TestCase):
+    events = [
+        Event(0, 3), Event(0, 1), Event(1, 2), Event(3, 5),
+        Event(1, 3), Event(1, 3), Event(1, 3), Event(3, 6),
+        Event(2, 7), Event(2, 3), Event(2, 7), Event(7, 9),
+        Event(3, 5), Event(2, 4), Event(2, 3), Event(3, 7),
+        Event(4, 5), Event(6, 7), Event(6, 9), Event(7, 9),
+        Event(8, 9), Event(4, 6), Event(8, 10), Event(7, 10)
+    ]
+
+    # Уровень A
+    def test_A_naive_calc(self):
+        events = [1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7]
+        interval = 1
+        self.assertEqual(20.5, sum(naive_calc(events, interval)))
+
+    def test_A_fast_calc(self):
+        events = [1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7]
+        interval = 1
+        self.assertEqual(20.5, sum(fast_calc(events, interval)))
+
+    # Уровень B
+    def test_B_0_10(self):
+        self.assertEqual(str(calcStartTimes(self.events, 0, 10)), "[(0, 1), (1, 2), (2, 3), (3, 5), (6, 7), (7, 9)]")
+
+    def test_B_1_7(self):
+        self.assertEqual(str(calcStartTimes(self.events, 1, 7)), "[(1, 2), (2, 3), (3, 5), (6, 7)]")
+
+    def test_B_2_8(self):
+        self.assertEqual(str(calcStartTimes(self.events, 2, 8)), "[(2, 3), (3, 5), (6, 7)]")
+
+    def test_B_5_10(self):
+        self.assertEqual(str(calcStartTimes(self.events, 5, 10)), "[(6, 7), (7, 9)]")
+
+    def test_B_2_6(self):
+        self.assertEqual(str(calcStartTimes(self.events, 2, 6)), "[(2, 3), (3, 5)]")
+
+    # Уровень C
+    def test_C_calcMaxCost(self):
+        items = [
+            Item(60, 20),
+            Item(100, 50),
+            Item(120, 30),
+            Item(100, 50)
+        ]
+
+        data=[
+            (0, 0.0),
+            (10, 40.0),
+            (20 ,80.0),
+            (30, 120.0),
+            (40, 150.0),
+            (50, 180.0),
+            (60, 200.0),
+            (70, 220.0),
+            (80, 240.0),
+            (90, 260.0)
+        ]
+        for inp in data:
+            fullCost = calcMaxCost(items, inp[0])
+            self.assertEqual(fullCost,inp[1])
---
+++ src/akhmelev/lesson03/b_huffman.py
@@ -0,0 +1,67 @@
+task='''
+ Lesson 3. B_Huffman.
+ Восстановите строку по её коду и беспрефиксному коду символов.
+
+ В первой строке входного файла заданы два целых числа
+ kk и ll через пробел — количество различных букв, встречающихся в строке,
+ и размер получившейся закодированной строки, соответственно.
+
+ В следующих kk строках записаны коды букв в формате "letter: code".
+ Ни один код не является префиксом другого.
+ Буквы могут быть перечислены в любом порядке.
+ В качестве букв могут встречаться лишь строчные буквы латинского алфавита;
+ каждая из этих букв встречается в строке хотя бы один раз.
+ Наконец, в последней строке записана закодированная строка.
+ Исходная строка и коды всех букв непусты.
+ Заданный код таков, что закодированная строка имеет минимальный возможный размер.
+
+        Sample Input 1:
+        1 1
+        a: 0
+        0
+
+        Sample Output 1:
+        a
+
+
+        Sample Input 2:
+        4 14
+        a: 0
+        b: 10
+        c: 110
+        d: 111
+        01001100100111
+
+        Sample Output 2:
+        abacabad
+'''
+
+def decode(filename):
+    f=open(filename)
+    count,lenght=map(int,f.readline().replace("\n","").split(" "))
+    d=dict()
+    for i in range(0,count):
+        ch,code=f.readline().replace("\n","").split(": ")
+        d.update({code:ch})
+    s=f.readline().replace("\n","")
+    buff=""
+    res=""
+    for symbol in s:
+        buff=buff+symbol
+        ch=d.get(buff)
+        if ch:
+            res=res+ch
+            buff=""
+    f.close()
+    return res
+
+def main():
+    res = decode("encodeHuffman.txt")
+    print(res)
+
+
+# Для ручной проверки нажмите Ctrl+Shift+F10
+# установив курсор на  main
+# или создайте конфигурацию Run-Edit configuration
+if __name__ == "__main__":
+    main()
