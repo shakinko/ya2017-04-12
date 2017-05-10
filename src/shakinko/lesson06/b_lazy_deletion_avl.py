d18fb0c5c20e720ceac1a513ef9a27cda59972b3
mitrich <mitrich@yandex-team.ru>
2017-05-10 03:35:46 +0300

    06a, 06b, 07a, 07b

--- src/shakinko/lesson06/b_lazy_deletion_avl.py
+++ src/shakinko/lesson06/b_lazy_deletion_avl.py
@@ -16,19 +16,21 @@ class AvlB():
             self.left = None
             self.right = None
             self.height = 0
+            # С каждым узлом ассоциирован флаг deleted
+            self.deleted = False
 
         def balance(self):
             return (self.left.height if self.left else -1) - (
                 self.right.height if self.right else -1)
 
         def __str__(self):
-            # return str(self.key)+"/"+str(self.balance())+"/"+str(self.height)
-            # return str(self.key)+"/"+str(self.height)
-            return str(self.key)
+            # узел с флагом deleted = True считаем удаленным по-ленивому
+            # и на печать ключ такого узла не выводим
+            return "." if self.deleted else str(self.key)
 
 
     def __init__(self, root=None):
-        self.root = root  # корень
+        self.root = self.copy_tree = root  # 2 корня, тот, который copy нужен для lazy deletion
         self._outlist = list()  # вспомогательный лист в котором строится дерево для вывода его на экран
         # for lazy deletion
         self.nodecount = 0
@@ -40,43 +42,118 @@ class AvlB():
         self._free(self.root)
 
     def add(self, key, value):
-        pass
+        self.root = self._add(self.root, key, value)
 
-    def delete(self,key):
-        pass
+    def delete(self, key):
+        # ищем узел
+        self.lookup(key)
+        # и просто включаем ему флаг deleted
+        # print(t)
+        self.deleted = True
+        self.delcount += 1
+
+        # коэффициент обновления = 0.5 хардкодим прямо здесь
+        # по-хорошему его надо бы вынести в константы
+        if self.nodecount * 0.5 <= self.delcount:
+            # создаем новое дерево, содержащее все не удалённые узлы
+            self.copy(self.root)
+#            self.root = self.copy_tree
 
     def copy(self, node):
-        pass
+        if node:
+            if not node.deleted:
+                self.copy_tree = self._add(self.copy_tree, node.key, node.value)
+
+            if node.left and not node.left.deleted:
+                self.copy_tree = self._add(self.copy_tree, node.left.key, node.left.value)
+
+            elif node.right and not node.right.deleted:
+                self.copy_tree = self._add(self.copy_tree, node.right.key, node.right.value)
+
+        # рекурсивно спускаемся по дереву и продолжаем копировать
+                self.copy(node.left)
+                self.copy(node.right)
 
     def lookup(self, key):
         return self._lookup(self.root, key)
 
     def _free(self, tree):
-        pass
+        if tree:
+            self._free(tree.left)
+            self._free(tree.right)
+            self.nodecount = 0
+            self.delcount = 0
+            tree = None
 
     def _lookup(self, tree, key):
-        pass
+        while tree:
+            if tree.key == key:
+                return tree
+            elif key < tree.key:
+                tree = tree.left
+            else:
+                tree = tree.right
+        return tree
 
     def _create(self, key, value):
-        pass
+        return self.Node(key, value)
 
     def _height(self, tree):
-        pass
+        return tree.height if tree else -1
 
     def _add(self, tree, key, value):
-        pass
+        if not tree:
+            self.root = self._create(key, value)
+            # Инкрементим счетчик, это нужно будет потом для lazy deletion
+            self.nodecount += 1
+            return self.root
+        if (key < tree.key):
+            tree.left = self._add(tree.left, key, value)
+            if self._height(tree.left) - self._height(tree.right) == 2:
+                if key < tree.left.key:
+                    tree = self._right_rotate(tree)
+                else:
+                    tree = self._leftright_rotate(tree)
+        elif (key > tree.key):
+            tree.right = self._add(tree.right, key, value)
+            if self._height(tree.right) - self._height(tree.left) == 2:
+                if key > tree.right.key:
+                    tree = self._left_rotate(tree)
+                else:
+                    tree = self._rightleft_rotate(tree)
+
+        else:
+            if tree.deleted:
+                tree.deleted = False
+                self.delcount -= 1
+            else:
+                tree.value = value
+        tree.height = max(self._height(tree.left), self._height(tree.right)) + 1
+        return tree
 
     def _right_rotate(self, tree):
-        pass
+        root = tree.left
+        tree.left = root.right
+        root.right = tree
+        tree.height = max(self._height(tree.left), self._height(tree.right)) + 1
+        root.height = max(self._height(root.left), tree.height) + 1
+        return root
 
     def _left_rotate(self, tree):
-        pass
+        root = tree.right
+        tree.right = root.left
+        root.left = tree
+        tree.height = max(self._height(tree.left), self._height(tree.right)) + 1
+        root.height = max(self._height(root.right), tree.height) + 1
+        return root
 
     def _leftright_rotate(self, tree):
-        pass
+        tree.left = self._left_rotate(tree.left)
+        return self._right_rotate(tree)
 
     def _rightleft_rotate(self, tree):
-        pass
+        tree.right = self._right_rotate(tree.right)
+        return self._left_rotate(tree)
 
     # обновление массива для строкового представления дерева
     def _dfs_print(self, tree, level):
