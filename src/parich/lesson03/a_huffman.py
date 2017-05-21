a36d6ab901ab765c39b642b4dc006ba66dbe6949
OlgaParich <olga.gonchar.1991@gmail.com>
2017-05-21 21:33:52 +0300

    Lesson03 task a,b

--- src/parich/lesson03/a_huffman.py
+++ src/parich/lesson03/a_huffman.py
@@ -35,7 +35,7 @@ Lesson 3. A_Huffman.
         d: 111
         01001100100111
 
-# Изучите классы Node, InternalNode, LeafNode - без 
+# Изучите классы Node, InternalNode, LeafNode - без
   полного понимания их работы вы не выполните это задание
 '''
 
@@ -45,6 +45,8 @@ class Node:
     # а только через его версии InternalNode и LeafNode
     def __init__(self, frequence):  # конструктор по умолчанию
         self.frequence = frequence  # частота символов
+        self.symbol = None  # FIX символ нужен для лексикографической
+        # сортировки при равенстве частот
 
     # генерация кодов (вызывается на корневом узле
     # вызывается один раз в конце, т.е.после построения дерева)
@@ -55,7 +57,10 @@ class Node:
 
     # компаратор узлов по частоте (нужен для heapq)
     def __lt__(self, other):
-        return self.frequence < other.frequence
+        if self.frequence != other.frequence:
+            return self.frequence < other.frequence
+        else:
+            return self.symbol > other.symbol  # FIX
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -68,13 +73,14 @@ class InternalNode(Node):
         super().__init__(left.frequence + right.frequence)
         self.left = left  # левый ребенок бинарного дерева
         self.right = right  # правый ребенок бинарного дерева
+        self.symbol = left.symbol  # копируем символ из левого листа (для корректной сортировки)
 
     def __repr__(self):
         return repr(("-", self.frequence))
 
     def fill_codes(self, code):
-        self.left.fill_codes(code + "0")
-        self.right.fill_codes(code + "1")
+        self.left.fill_codes(code + "1")
+        self.right.fill_codes(code + "0")
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -83,7 +89,8 @@ class InternalNode(Node):
 class LeafNode(Node):
     def __init__(self, symbol, frequence):
         super().__init__(frequence)
-        self.symbol = symbol  # символы хранятся только в листах
+        self.symbol = symbol    # символы хранятся в листах и дублируются
+                                # в вершины для лексикографической сортировки
 
     # добрались до листа, значит рекурсия закончена, код уже готов
     # и можно запомнить его в индексе для поиска кода по символу.
@@ -93,15 +100,9 @@ class LeafNode(Node):
     def __repr__(self):
         return repr((self.symbol, self.frequence))
 
-    # компаратор листьев (нужен для heapq)
-    def __lt__(self, other):
-        if self.frequence == other.frequence:
-            print(self.symbol, other.symbol, self.symbol > other.symbol)
-            return self.symbol > other.symbol
-        return super().__lt__(other)
 
-#очередь, выделена в отдельный
-#класс для удобного использования
+# очередь, выделена в отдельный
+# класс для удобного использования
 
 class Heap():
     def __init__(self) -> None:
@@ -125,25 +126,50 @@ codes = dict()
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
 def encode(filename):
-    # все комментарии от тестового решения были оставлены т.к. это задание A.
-    # если они вам мешают их можно удалить
-
     # 1. переберем все символы по очереди и рассчитаем их частоту в словаре counts
     # для каждого символа добавим 1 если элемента еще нет или инкремент если есть.
-
+    f = open(filename)
+    string = f.readline()
+    counts = dict()
+    for ch in string:
+        count = counts.get(ch)
+        if count:
+            counts.update({ch: count+1})
+        else:
+            counts.update({ch: 1})
     # 2. перенесем все символы в приоритетную очередь в виде листьев
-
+    h = Heap()
+    for sym, count in counts.items():
+        leaf = LeafNode(sym, count)
+        h.push(leaf)
     # 3. вынимая по два узла из очереди (для сборки родителя)
     # и возвращая этого родителя обратно в очередь
     # построим дерево кодирования Хаффмана
-
+    # все комментарии от тестового решения были оставлены т.к. это задание A.
+    # если они вам мешают их можно удалить
+    while h.size() > 1:
+        left = h.pop()
+        right = h.pop()
+        parent = InternalNode(left, right)
+        h.push(parent)
     # 4. последний из родителей будет корнем этого дерева
     # это будет последний и единственный элемент оставшийся в очереди.
+    root = h.pop()
 
+    if len(counts) == 1:
+        root.fill_codes("0")
+    else:
+        root.fill_codes("")
+
+    # for ch, code in codes.items():
+    #     print(ch, ":", code)
     # 5. Можно кодировать. Для этого нужно получить коды и составить словарь
+    res = ""
+    for ch in string:
+        res = res + codes.get(ch)
+    return res
 
-    return
-
+# encode("F:\python\ya2017-04-12\src\moskalenko\lesson03\dataHuffman.txt")
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
 
@@ -158,3 +184,4 @@ def main():
 # или создайте конфигурацию Run-Edit configuration
 if __name__ == "__main__":
     main()
+
